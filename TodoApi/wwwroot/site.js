04fa943825aa95b02d7a7e2f40e62be49094c2a4
BlueSkyCaps <linsongxian@outlook.com>
2020-04-17 12:21:45 +0800

    upload the project

---
+++ TodoApi/wwwroot/site.js
@@ -0,0 +1,137 @@
+﻿const uri = "api/todo";
+let todos = null;
+function getCount(data) {
+    const el = $("#counter");
+    let name = "to-do";
+    if (data) {
+        if (data > 1) {
+            name = "to-dos";
+        }
+        el.text(data + " " + name);
+    } else {
+        el.text("No " + name);
+    }
+}
+// 指定在DOM完全加载时要执行的函数。
+$(document).ready(function () {
+    getData();
+});
+
+function getData() {
+    $.ajax({
+        type: "GET",
+        url: uri,
+        cache: false,
+        success: function (data) {
+            const tBody = $("#todos");
+            
+            $(tBody).empty(); // 从DOM中删除匹配元素集的所有子节点。
+
+            getCount(data.length);
+
+            // .append()方法前面的选择器表达式是将内容插入其中的容器
+            $.each(data, function (key, item) {
+                const tr = $("<tr></tr>")
+                    .append(
+                        $("<td></td>").append(
+                            $("<input/>", {
+                                type: "checkbox",
+                                disabled: true,
+                                checked: item.isComplete
+                            })
+                        )
+                    )
+                    .append($("<td></td>").text(item.name))
+                    .append(
+                        $("<td></td>").append(
+                            $("<button>Edit</button>").on("click", function () {
+                                editItem(item.id);
+                            })
+                        )
+                    )
+                    .append(
+                        $("<td></td>").append(
+                            $("<button>Delete</button>").on("click", function () {
+                                deleteItem(item.id);
+                            })
+                        )
+                    );
+
+                tr.appendTo(tBody); // 其被插入到目标容器。
+            });
+
+            todos = data;
+        }
+    });
+}
+
+function addItem() {
+    const item = {
+        name: $("#add-name").val(),
+        isComplete: false
+    };
+
+    // POST新增 不需要包含实体的全部可空属性 主键ID会自动生成
+    $.ajax({
+        type: "POST",
+        accepts: "application/json",
+        url: uri,
+        contentType: "application/json",
+        data: JSON.stringify(item), // 将JavaScript对象或值转换为JSON字符串
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert("Something went wrong!");
+        },
+        success: function (result) {
+            getData();
+            $("#add-name").val("");
+        }
+    });
+}
+
+function deleteItem(id) {
+    $.ajax({
+        url: uri + "/" + id,
+        type: "DELETE",
+        success: function (result) {
+            getData();
+        }
+    });
+}
+
+function editItem(id) {
+    $.each(todos, function (key, item) {
+        if (item.id === id) {
+            $("#edit-name").val(item.name);
+            $("#edit-id").val(item.id);
+            $("#edit-isComplete")[0].checked = item.isComplete;
+        }
+    });
+    $("#spoiler").css({ display: "block" });
+}
+
+$(".my-form").on("submit", function () {
+    const item = {
+        name: $("#edit-name").val(),
+        // .is() 这些元素中至少有一个与给定参数匹配返回true。选择器 ":checked" 匹配所有选中的元素。
+        isComplete: $("#edit-isComplete").is(":checked"), 
+        id: $("#edit-id").val()
+    };
+    // PUT更新 必须包含整个实体
+    $.ajax({
+        url: uri + "/" + $("#edit-id").val(),
+        type: "PUT",
+        accepts: "application/json",
+        contentType: "application/json",
+        data: JSON.stringify(item),
+        success: function (result) {
+            getData();
+        }
+    });
+
+    closeInput();
+    return false;
+});
+
+function closeInput() {
+    $("#spoiler").css({ display: "none" });
+}
